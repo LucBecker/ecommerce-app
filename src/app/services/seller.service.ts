750b5c3716c8f3b36b4fe87854121de20278d4eb
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-07-02 12:28:18 -0300

    service for user login

--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { signUp } from '../data-type';
-import { Route, Router } from '@angular/router';
+import { EventEmitter, Injectable } from '@angular/core';
+import { login, signUp } from '../data-type';
+import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
@@ -10,6 +10,7 @@ import { BehaviorSubject } from 'rxjs';
 export class SellerService {
 
   isSellerLoggedIn = new BehaviorSubject<boolean>(false);
+  isLoginError  = new EventEmitter<boolean>(false);
 
   constructor(
     private http:HttpClient,
@@ -33,5 +34,19 @@ export class SellerService {
       this.isSellerLoggedIn.next(true);
       this.router.navigate(['seller-home']);
     }
+  }
+
+  userLogin(data:login){
+    this.http.get(`http://localhost:3000/seller?email=${data.email}&password=${data.password}`,
+    {observe: 'response'}).subscribe((result: any) => {
+      if(result && result.body && result.body.length===1){
+        this.isLoginError.emit(false);
+        localStorage.setItem('seller', JSON.stringify(result.body));
+        this.router.navigate(['seller-home']);
+      }else{
+        console.warn('login failed');
+        this.isLoginError.emit(true);
+      }
+    })
   }
 }
