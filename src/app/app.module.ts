38b5f0dc81180b20fa2fd9792f76634253d18dc0
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-07-01 23:52:06 -0300

    reafctor files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { SellerAuthComponent } from './seller-auth/seller-auth/seller-auth.component';
-import { HomeComponent } from './home/home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
