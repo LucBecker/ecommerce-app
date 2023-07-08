ec4137fca58b994dbbe90558c4f6b33801acd9c8
Lucas Becker <lks.becker@gmail.com>
2023-07-07 23:44:38 -0300

    user auth component

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { signUp } from '../data-type';
 
 @Component({
   selector: 'app-user-auth',
   templateUrl: './user-auth.component.html',
   styleUrls: ['./user-auth.component.css']
 })
-export class UserAuthComponent {
+export class UserAuthComponent implements OnInit {
+
+  constructor(){}
+
+  ngOnInit(): void {}
+
+  signUp(data:signUp){
+    console.warn(data);
+  }
 
 }
