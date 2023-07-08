0079ca1d62086d3398c8278b2e653170ede9267b
Lucas Becker <lks.becker@gmail.com>
2023-07-07 22:52:19 -0300

    adding route product details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { SellerHomeComponent } from './seller-home/seller-home.component';
 import { SellerAddProductComponent } from './seller-add-product/seller-add-product.component';
 import { SellerUpdateProductComponent } from './seller-update-product/seller-update-product.component';
 import { SearchComponent } from './search/search.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 const routes: Routes = [
   {
@@ -35,6 +36,10 @@ const routes: Routes = [
   {
     path:'search/:query',
     component: SearchComponent
+  },
+  {
+    path:'details/:productId',
+    component: ProductDetailsComponent
   }
 ];
 
