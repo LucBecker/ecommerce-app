fa4fc3b05712b06cea6c33980266e32838bf1387
Lucas Becker <lks.becker@gmail.com>
2023-07-16 23:16:55 -0300

    service function deleteCartItems

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -96,4 +96,10 @@ export class ProductService {
     let userData = userStore && JSON.parse(userStore);
     return this.http.get<order[]>('http://localhost:3000/orders?userId=' + userData.id);
   }
+
+  deleteCartItems(cartId: number) {
+    return this.http.delete('http://localhost:3000/cart/' + cartId).subscribe((result) => {
+      this.cartData.emit([]);
+    })
+  }
 }
