13482d887adf765a5f443b47260f66acd12d0870
Lucas Becker <lks.becker@gmail.com>
2023-07-16 23:29:26 -0300

    call cancel order api

--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -9,13 +9,23 @@ import { ProductService } from '../services/product.service';
 })
 export class MyOrdersComponent implements OnInit {
 
-  orderData:order[]|undefined
+  orderData: order[] | undefined
+
   constructor(private product:ProductService) { }
 
   ngOnInit(): void {
+    this.getOrderList()
+  }
+  cancelOrder(orderId:number|undefined){
+    orderId && this.product.cancelOrder(orderId).subscribe((result)=>{
+      if(result){
+        this.getOrderList();
+      }
+    })
+  }
+  getOrderList(){
     this.product.orderList().subscribe((result)=>{
       this.orderData=result;
     })
   }
-
 }
